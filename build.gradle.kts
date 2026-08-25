cb6c2c7aa8b83da82cc0623034cc54727b3aaf26
Dorino Dev <dev@dorino.app>
2026-08-25 07:09:44 +0000

    fix: استفاده از الگوی استاندارد plugins{} با apply false در build.gradle.kts ریشه

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,7 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.24")
-    }
+plugins {
+    id("com.android.application") version "8.4.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.24" apply false
 }
 
 tasks.register("clean", Delete::class) {
